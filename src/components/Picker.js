1e86ce72ab8974cd8b61c58e77f69ca898eef132
Subrata Sarker <subrata_sarker@yahoo.com>
2019-04-24 09:40:28 +0000

    Update the Picker with a title

--- src/components/Picker.js
+++ src/components/Picker.js
@@ -2,17 +2,20 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Picker = ({ value, onChange, options }) => (
-  <span>
-    <h1>{value}</h1>
-    <select onChange={e => onChange(e.target.value)}
-            value={value}>
-      {options.map(option =>
-        <option value={option} key={option}>
-          {option}
-        </option>)
-      }
-    </select>
-  </span>
+  <div>
+    <h1>Redux Async + CircleCI</h1>
+    <span>
+      <h3>{value}</h3>
+      <select onChange={e => onChange(e.target.value)}
+              value={value}>
+        {options.map(option =>
+          <option value={option} key={option}>
+            {option}
+          </option>)
+        }
+      </select>
+    </span>
+  </div>
 )
 
 Picker.propTypes = {
